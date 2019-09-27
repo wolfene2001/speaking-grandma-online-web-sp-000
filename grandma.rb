3ad60d598c19ab4802971f798d1ffce8d8c7f51e
wolfene2001 <wolfene2001@yahoo.com>
2019-09-27 00:03:02 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ def speak_to_grandma(string)
   if string == string.downcase
     puts 'HUH?! SPEAK UP, SONNY!'
   elsif string == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif string == string.upcase
     puts "NO, NOT SINCE 1938!"
   end
