e202c53185f9343a80217e17c282698ebd8fa936
wolfene2001 <wolfene2001@yahoo.com>
2019-09-27 00:12:44 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,5 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(string)
-#  if string.downcase == string
-#    return 'HUH?! SPEAK UP, SONNY!'
   if string == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   elsif string.upcase == string
